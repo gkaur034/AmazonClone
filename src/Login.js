a2e08735b51f0a67f4d16aaf7534c09bbbf22f70
Gurleen Kaur Rahi <rahig@mcmaster.ca>
2025-05-23 15:09:26 -0400

    login page in progress

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import './Login.css';
 
 function Login() {
+  const [email, setEmail] = useState('');
   return (
     <div className='login'>
         <Link to="/">
@@ -12,6 +13,26 @@ function Login() {
                 alt='Amazon logo'
             />
         </Link>
+
+        <div className='login__container'>
+          <h1>Sign-in</h1>
+          <form>
+            <h5>Email</h5>
+            <input type='text' />
+
+            <h5>Password</h5>
+            <input type='password' />
+
+            <button className='login__signInButton'>Sign In</button>
+          </form>
+
+          <p>
+            By signing-in you agree to AMAZON FAKE CLONE Conditions of Use & Sale. Please see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
+          </p>
+
+          <button className='login__registerButton'>Create your amazon account</button>
+          
+        </div>
     </div>
   );
 }
