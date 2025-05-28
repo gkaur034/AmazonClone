47fa1fc4e3aad2f0f8fb5f760dc4e754563109dd
Gurleen Kaur Rahi <rahig@mcmaster.ca>
2025-05-28 15:14:18 -0400

    last min modifs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
+import { useEffect } from 'react';
 import './App.css';
 import Checkout from './Checkout';
 import Header from './Header';
 import Home from './Home';
 import Login from './Login';
+import {auth} from './firebase';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useStateValue } from './StateProvider';
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+  useEffect(() => {
+
+    auth.onAuthStateChanged(authUser => {
+      console.log('User is >>>', authUser);
+
+      if (authUser) {
+        
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
+        })
+      } else {
+
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+
+      }
+    } )
+  }, [])
+
   return (
     <BrowserRouter>
       <div className="App">
